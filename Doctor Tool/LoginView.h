7eaf9223df469e11b0b45eb55794a5f530cb886d
German Pereyra <german.f.pereyra@gmail.com>
2014-06-25 20:51:11 -0300

    LoginView added

---
+++ Doctor Tool/LoginView.h
@@ -0,0 +1,13 @@
+//
+//  LoginView.h
+//  Doctor Tool
+//
+//  Created by German Pereyra on 6/25/14.
+//  Copyright (c) 2014 Ponja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginView : UIView
+
+@end
