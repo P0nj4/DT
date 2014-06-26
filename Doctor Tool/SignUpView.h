15f0787fbb7385c21239093508a1a553f8df8c9e
German Pereyra <german.f.pereyra@gmail.com>
2014-06-25 21:00:53 -0300

    Refactor

---
+++ Doctor Tool/SignUpView.h
@@ -0,0 +1,13 @@
+//
+//  SignUpView.h
+//  Doctor Tool
+//
+//  Created by German Pereyra on 6/25/14.
+//  Copyright (c) 2014 Ponja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpView : UIView
+
+@end
---
+++ Doctor Tool/SignUpView.m
@@ -0,0 +1,31 @@
+//
+//  SignUpView.m
+//  Doctor Tool
+//
+//  Created by German Pereyra on 6/25/14.
+//  Copyright (c) 2014 Ponja. All rights reserved.
+//
+
+#import "SignUpView.h"
+
+@implementation SignUpView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
